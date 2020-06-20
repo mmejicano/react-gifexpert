e44388aa9514ce09dec46dd8b43efe42e6d4c3c9
Marvin Mejicano <mejicanomarvin@gmail.com>
2020-06-20 10:02:28 -0600

    docs agregado

---
+++ docs/precache-manifest.123de328f49a9b3297a70bd9c1ef7623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251359011ab5a21c0a3cc6ea19e0496b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12bbaa1952bd9cce58a4",
+    "url": "/static/css/main.973206a9.chunk.css"
+  },
+  {
+    "revision": "496f528a92b3c1e745d3",
+    "url": "/static/js/2.6d4f56ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d4f56ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12bbaa1952bd9cce58a4",
+    "url": "/static/js/main.8c6b1aa7.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
